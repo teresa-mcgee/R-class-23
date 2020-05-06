69dda9c7b3fb8af9ad98edb273e77c0aedd8055c
Dayne <dayne.filer@gmail.com>
2020-05-06 17:01:47 -0400

    Cleanup to pass R check

---
+++ R/dataObjects.R
@@ -0,0 +1,30 @@
+#' @name primers
+#' @title Messy primers data
+#' @description A 1-column data.frame with primer strings
+#' @details The data is intentionally "messy" to demonstrate issues with
+#' file loading, and how to clean data.
+
+NULL
+
+#' @name AAPL
+#' @title AAPL stock price data
+#' @description AAPL stock price data
+
+NULL
+
+#' @name yeast
+#' @title Time-series data for three yeast strains
+#' @description Time-series data for three yeast strains
+#' @details In wide format with strain in a different column. 
+#' Provided by Amy Pomeroy
+
+NULL
+
+#' @name yeast_long
+#' @title Long-format time-series data for three yeast strains
+#' @description Long-format time-series data for three yeast strains
+#' @details In long format with strain given in the 'Strain' column. 
+#' Provided by Amy Pomeroy
+
+NULL
+
